933cc087c0ff3cd8d4dd44ce5c40f345648c1239
Marcos Rufino <marcosrufino@Rufino-MacBook-Pro.local>
2020-03-03 19:55:43 -0300

    first commit

---
+++ src/main/java/com/desafio1/desafio01/Desafio01Application.java
@@ -0,0 +1,15 @@
+package com.desafio1.desafio01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Desafio01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio01Application.class, args);
+	}
+
+
+}
